e6603d4dd721db08f37956f6d5361a2af6abc5e7
Daniela Giacomelli <danielafgiacomelli@gmail.com>
2024-02-21 16:06:39 -0300

    Templates html

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse    
+#from django.http import HttpResponse    
 
 def home(request):
     return render(request, 'recipes/home.html',context={
         'name' : 'Daniela Giacomelli',
     })
-
-def sobre(request):
-    return HttpResponse('Sobre: ')
-
-def contato(request):
-    return HttpResponse('Daniela Giacomelli - (64) 9999-0000')
