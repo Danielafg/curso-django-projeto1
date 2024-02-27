809cc85dfb74c8be90ebee9e93cb5f8c2385f2af
Daniela Giacomelli <danielafgiacomelli@gmail.com>
2024-02-27 08:26:37 -0300

    configurando urls

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html',context={
         'name' : 'Daniela Giacomelli',
     })
+
+def recipe(request,id):
+    return render(request, 'recipes/pages/home.html',context={
+        'name' : 'Daniela Giacomelli',
+    })
